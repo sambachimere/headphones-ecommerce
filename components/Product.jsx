6e5c5dd2d414d8012d0642599498644df72de7cc
Samba Diaw <sambachimerediaw@gmail.com>
2022-05-02 14:15:27 +0200

    improve ui

--- components/Product.jsx
+++ components/Product.jsx
@@ -8,7 +8,7 @@ const Product = ({ product: { image, name, slug, price } }) => {
     <div>
       <Link href={`/product/${slug.current}`}>
         <div className="product-card">
-          <img src={urlFor(image && image[0])} width={250} height={250} className="product-image" />
+          <img src={urlFor(image && image[0])} className="product-image" />
           <p className="product-name">{name}</p>
           <p className="product-price">${price}</p>
         </div>
